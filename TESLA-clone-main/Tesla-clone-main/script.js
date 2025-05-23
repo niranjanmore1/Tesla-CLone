19fd73bfd573f6d8b708b65c45e2b5116500ae01
NIRANJAN MORE <114484250+niranjanmore1@users.noreply.github.com>
2025-05-23 21:55:48 +0530

    Add files via upload

---
+++ TESLA-clone-main/Tesla-clone-main/script.js
@@ -0,0 +1,17 @@
+const menu = document.querySelector(".Menu");
+const cancel = document.querySelector(".menu-cross");
+const menubar = document.querySelector(".menubar");
+const body = document.querySelector(".body");
+
+/
+menu.addEventListener("click", () => {
+  // console.log("hello");
+  menubar.classList.toggle("active");
+  body.style.backgroundColor = "#cfcfcf";
+});
+cancel.addEventListener("click", () => {
+  // console.log("hello");
+  menubar.classList.toggle("active");
+  // body.style.backgroundColor = "gray";
+  //
+});
